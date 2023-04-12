2e188827c49b7ea33daa2735aa0cb5bb16b36f3a
Tamas Mako <mako.tomi@gmail.hu>
2023-04-12 20:22:38 +0200

    header section

--- src/app.js
+++ src/app.js
@@ -2,3 +2,35 @@
 import './index.html';
 
 import './style.css';
+
+document.addEventListener('DOMContentLoaded', () => {
+  const menu = document.querySelector('#menu');
+  menu.addEventListener('click', () => {
+    menu.classList.toggle('fa-times');
+    document.querySelector('header').classList.toggle('toggle');
+  });
+
+  function toggleMenuClasses() {
+    menu.classList.remove('fa-times');
+    document.querySelector('header').classList.remove('toggle');
+  }
+
+  const topElement = document.querySelector('.top');
+  function toggleTopDisplay() {
+    if (window.pageYOffset > 0) {
+      topElement.style.display = 'block';
+    } else {
+      topElement.style.display = 'none';
+    }
+  }
+
+  window.addEventListener('scroll', () => {
+    toggleMenuClasses();
+    toggleTopDisplay();
+  });
+
+  window.addEventListener('load', () => {
+    toggleMenuClasses();
+    toggleTopDisplay();
+  });
+});
